e9711ac69c89c17b8b389a1a8e0ef3db55ad0f39
lfvperes <luisfvperes@gmail.com>
2025-12-12 16:41:57 -0300

    refactor: :recycle: cleaning up code

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,23 +1,22 @@
-
 roman = "IVXLCDM"
 def decimal_to_roman(N: int) -> str:
-    answer: str = ""
-    if N < 40:
-        count_tens = N // 10
-        count_fives = (N - count_tens * 10) // 5
+    count_tens = N // 10
+    if count_tens < 4:
+        count_fives = (N % 10) // 5
         count_ones = N % 5
         tens = count_tens*"X"
         fives = count_fives*"V"
+        ones = count_ones*"I"
+        answer = f"{tens}"
         if count_ones == 4:
             if count_fives == 0:
-                answer += f"{tens}IV"
+                answer += "IV"
             elif count_fives == 1:
-                answer += f"{tens}IX"
+                answer += "IX"
         else:
-            ones = count_ones*"I"
-            answer += f"{tens}{fives}{ones}"
+            answer += f"{fives}{ones}"
     else:
-        answer = "".join([roman[roman.find(r)+2] for r in decimal_to_roman(N//10)]) + decimal_to_roman(N%10)
+        answer = "".join([roman[roman.find(r)+2] for r in decimal_to_roman(count_tens)]) + decimal_to_roman(N%10)
     
     return answer
 

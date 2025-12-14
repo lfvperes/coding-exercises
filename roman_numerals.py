952c62280709914a00b7cb1689841b843e05a844
lfvperes <luisfvperes@gmail.com>
2025-12-13 21:54:48 -0300

    feat: :sparkles: reverse conversion
    adding conversion from roman to decimal and tests

--- roman_numerals.py
+++ roman_numerals.py
@@ -20,6 +20,31 @@ def decimal_to_roman(N: int) -> str:
     
     return answer
 
-for i in range(2995,3005):
+roman_examples = []
+for i in range(2994,3005):
     answer = decimal_to_roman(i)
+    roman_examples.append(answer)
     print(f"{i}={answer}")
+
+def roman_to_decimal(R: str) -> int:
+    answer: int = 0
+    size: int = len(R)
+    if size == 1:
+        index: int = roman.find(R)
+        multiplier: int = 5 ** (index % 2)
+        zeros: int = index // 2
+        answer += multiplier * 10 ** zeros
+    else:
+        cur = R[0]
+        nxt = R[1]
+        if roman.find(cur) < roman.find(nxt):
+            answer += roman_to_decimal(nxt) - roman_to_decimal(cur)
+            if size > 2:
+                answer += roman_to_decimal(R[2:])
+        else:
+            answer += roman_to_decimal(cur) + roman_to_decimal(R[1:])
+
+    return answer
+
+for r in roman_examples:
+    print(f"{r}={roman_to_decimal(r)}")
